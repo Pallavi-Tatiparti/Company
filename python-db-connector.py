5255fe82e07dddb514c2f3f607e4f6582037c0a5
Pallavi-Tatiparti <ptatiparti@gmail.com>
2020-12-02 20:46:59 +0530

    Update python-db-connector.py

--- python-db-connector.py
+++ python-db-connector.py
@@ -1,21 +1,22 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="root123",database="credence")
+Mydb=mysql.connector.connect(host="localhost",user="root",passwd="root123",database="credence")
 
-mycursor=mydb.cursor()
+Mycursor=Mydb.cursor()
+Mycursor = Mydb.cursor(buffered=True)
 
-mycursor.execute("SELECT * FROM imageblob")
+Mycursor.execute("SELECT * FROM Picture ")
 
 def Insertblob(FilePath):  #insert image into db code
 	with open(FilePath,"rb") as File:
 		BinaryData=File.read()
-	SqlStmt="INSERT INTO imageblob (image) VALUES (%s)"
-	mycursor.execute(SqlStmt,(BinaryData))
-	mydb.commit()
+	SqlStmt="INSERT INTO Picture(photo) VALUES (%s)"
+	Mycursor.execute(SqlStmt,(BinaryData,))
+	Mydb.commit()
 
-def Retrieveblob(ID):
-	SqlStmt2="SELECT * FROM imageblob WHERE id='{0}'"
-	mycursor.execute(SqlStmt2.format(str(ID)))
-	MyRes=mycursor.fetchone()[1]
+def Retrieveblob(ID): #retrieve img
+	SqlStmt2="SELECT * FROM Picture WHERE id='{0}'"
+	Mycursor.execute(SqlStmt2.format(str(ID)))
+	MyRes=Mycursor.fetchone()[1]
 	StoreFilePath="ImageOutputs/img{0}.jpg".format(str(ID))
 	print(MyRes)
 	with open(StoreFilePath,"wb")as File:
@@ -23,10 +24,11 @@ def Retrieveblob(ID):
 		File.close()
 
 
-print("1. Insert image \n 2.Read image")
+print("1. Insert image\n2. Read image")
 MenuInput=input()
-if int(MenuInput) ==1:
-	UserFilepath==input("Enter file path:")
+if int(MenuInput)==1:
+	UserFilepath=input("Enter file path:")
+	Insertblob(UserFilepath)
 elif int(MenuInput)==2:
 	UserIDchoice= input("Enter id:")
 	Retrieveblob(UserIDchoice)
